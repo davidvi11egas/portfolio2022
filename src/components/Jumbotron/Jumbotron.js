89e66a4c2964678ebbc52ae99a448cb40ac14a00
davidvi11egas <davidvillegas627@gmail.com>
2022-06-01 22:09:51 -0400

    added new hero component

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -2,9 +2,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './Jumbotron.css';
 
-const Jumbotron = () => (
-
-
+const Jumbotron = () => ( 
+    <h1>jumbo</h1>
 );
 
 export default Jumbotron;
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,13 @@
 import './Home.css';
 import Jumbotron from '../../components/Jumbotron';
-
+import Hero from '../../components/Hero';
 const Home = () => (
-<div className="container">
-<Jumbotron/>
+<div className="container-fluid">
+<Hero backgroundImage="https://cdn.pixabay.com/photo/2016/11/29/05/01/beach-1867436_1280.jpg">
+      <h1 className="pageTitle">David Villegas</h1>
+      <h2>Web Developer & Digital Marketing Professional</h2>
+
+    </Hero>
 
     <h1>Home</h1>
 </div>
